4103886cc5f53cf954493c67f476cb8bd7d9e4b1
Claudio Araujo <77213711+Tlakuache123@users.noreply.github.com>
2024-10-29 22:27:50 -0600

    add: Initial commit

---
+++ wezterm.lua
@@ -0,0 +1,78 @@
+local wezterm = require("wezterm")
+local config = {}
+local launch_menu = {}
+local act = wezterm.action
+
+config.default_prog = { "powershell.exe", "-NoLogo" }
+
+table.insert(launch_menu, {
+	label = "PowerShell",
+	args = { "powershell.exe", "-NoLogo" },
+})
+table.insert(launch_menu, {
+	label = "Pwsh",
+	args = { "pwsh.exe", "-NoLogo" },
+})
+
+config.font = wezterm.font("JetBrains Mono")
+config.color_scheme = "Catppuccin Mocha"
+config.window_padding = {
+	left = 4,
+	right = 4,
+	top = 4,
+	bottom = 0,
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.6
+config.win32_system_backdrop = "Acrylic"
+config.integrated_title_buttons = {}
+-- Tab settings
+config.use_fancy_tab_bar = false
+config.tab_max_width = 12
+config.tab_bar_at_bottom = true
+
+-- Tab style
+--[[
+config.colors = {
+	tab_bar = {
+		background = "#282828",
+	},
+}
+]]
+--
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab_title(tab)
+	title = wezterm.truncate_right(title, max_width - 2)
+	return {
+		{ Text = " " .. title .. " " },
+	}
+end)
+
+config.launch_menu = launch_menu
+
+config.keys = {
+	-- Create a new tab in the same domain as the current pane.
+	-- This is usually what you want.
+	{
+		key = "t",
+		mods = "SHIFT|ALT",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+}
+
+return config
